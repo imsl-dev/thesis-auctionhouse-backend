b4e10d321f4691bdaaecaf91302dfd6ff6f55d4f
Ignacio Solis Luna <solis.luna.ignacio@gmail.com>
2025-05-30 17:30:43 -0300

    project setup and enabled devtools

--- src/main/java/com/ps/auction/house/services/UserService.java
+++ src/main/java/com/ps/auction/house/services/UserService.java
@@ -2,6 +2,7 @@ package com.ps.auction.house.services;
 
 import com.ps.auction.house.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -12,29 +13,34 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
+@Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
 
-    public Boolean saveUserImage(Long userId, MultipartFile imageFile) {
-
-        try {
-            //create directory to save if it doesn't exist
-            File dir = new File(uploadDir);
-
-            if (!dir.exists()) {dir.mkdirs();}
-
-            //create unique file name
-
-            String fileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
-            Path filePath = Paths.get(uploadDir, fileName);
-
-            Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return true;
+    public String testMethod() {
+        return "Working as intended.";
     }
+//
+//    public Boolean saveUserImage(Long userId, MultipartFile imageFile) {
+//
+//        try {
+//            //create directory to save if it doesn't exist
+//            File dir = new File(uploadDir);
+//
+//            if (!dir.exists()) {dir.mkdirs();}
+//
+//            //create unique file name
+//
+//            String fileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
+//            Path filePath = Paths.get(uploadDir, fileName);
+//
+//            Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return true;
+//    }
 }
